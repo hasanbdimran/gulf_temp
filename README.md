1721fc8164bc3522892636f2fd567e8e3566a756
Mohammad Imran Hasan <hasanbdimran@gmail.com>
2024-10-28 00:25:59 -0500

    geotiff enabled

--- README.md
+++
@@ -1,1 +0,0 @@
-# gulf_temp
--- map.js
+++ map.js
@@ -58,15 +58,68 @@ var bounds = new L.LatLngBounds(
     new L.LatLng(22.896634, -87.769914));
 map.fitBounds(bounds);
 
-var urlPrefix = "/images/"
+var urlPrefix = "/images_tif/"
 
-var url = urlPrefix+dateTimes[0]+".png"
+var url = urlPrefix+dateTimes[0]+".tif"
 
-var imageOverlay = new L.ImageOverlay(url, bounds, {
-    opacity: 1.0,
-    interactive: true,
+const colorScale = chroma.scale(['#042333', '#40349F', '#8B538D', '#FCA63C', '#E8FA5B']).domain([23, 38]);
+// function(pixelValues) {
+//     var pixelValue = pixelValues[0]; // there's just one band in this raster
 
-}).addTo(map);
+//     // if there's zero wind, don't return a color
+//     if (isNaN(pixelValue)) return null;
+
+//     var color = colorScale(pixelValue).hex();
+//     return color;
+//   }:
+let image_layer = L.layerGroup();
+let key_track = []
+var color_sc = plotty.addColorScale("mycolorscale", ['#042333', '#40349F', '#8B538D','#D66C6C','#FCA63C', '#E8FA5B',], 
+    [0, 0.2, 0.4, 0.53333333, 0.66666667, 1]);
+// '#E8FA5B', '#FCA63C', '#D66C6C','#8B538D','#40349F', '#042333'
+// color_sc.colorScale.setNoDataValue(null);
+const options = {
+displayMin: 23,
+displayMax: 38,
+applyDisplayRange: false,
+clampLow: true,
+clampHigh: true,
+//   // Optional. Plotty color scale used to render the image.
+  colorScale: 'mycolorscale'
+
+};
+// const renderer = L.LeafletGeotiff.plotty(options);
+// renderer.setDisplayRange([23, 38]);
+// renderer.setNoDataValue(NaN);
+// console.log(renderer);
+const option_render = {
+
+    noDataValue:-9999,
+    renderer: L.LeafletGeotiff.plotty(options)
+}
+  
+
+    function imageUpdate(url) {
+        // If a polyline already exists, remove it
+        // if (image_layer) {
+        //     map.removeLayer(image_layer);
+        // }
+        image_layer = L.leafletGeotiff(url, option_render).addTo(map);
+        // image_layer.addTo(map);
+        const layerCount = Object.keys(map._layers).length;
+        console.log('layer count', layerCount);
+    };
+
+    imageUpdate(url);
+
+
+// var imageOverlay = imageUpdate(url);
+
+// var imageOverlay = new L.ImageOverlay(url, bounds, {
+//     opacity: 1.0,
+//     interactive: true,
+
+// }).addTo(map);
 
 
 //function when sliding
@@ -74,7 +127,8 @@ slider.oninput = function() {
     //changing the label
     document.getElementById("sliderLabel").innerHTML = dateTimes[this.value-1]
     //setting the url of the overlay
-    imageOverlay.setUrl(urlPrefix+dateTimes[this.value-1]+".png")
+    // imageOverlay.setUrl(urlPrefix+dateTimes[this.value-1]+".tif")
+    imageUpdate(urlPrefix+dateTimes[this.value-1]+".tif")
   }
   
   var playTimeOut;
@@ -99,7 +153,8 @@ slider.oninput = function() {
           //changing the label
           document.getElementById("sliderLabel").innerHTML = dateTimes[Number(val)-1]
           //setting the url of the overlay
-          imageOverlay.setUrl(urlPrefix+dateTimes[Number(val)-1]+".png")
+        //   imageOverlay.setUrl(urlPrefix+dateTimes[Number(val)-1]+".tif")
+          imageUpdate(urlPrefix+dateTimes[Number(val)-1]+".tif")
   
       }, 500);
   }
@@ -122,3 +177,46 @@ slider.oninput = function() {
   
   //hidding the stop button by default
   document.getElementById('stop').style.display = "none";
+
+  //Colorbar manupulation
+  function getColor(d) {
+    return d > 38 ? '#E8FA5B' :
+           d > 35 ? '#FCA63C' :
+           d > 32 ? '#D66C6C' :
+           d > 29 ? '#8B538D' :
+           d > 26 ? '#40349F' :
+                    '#042333';
+}
+
+var legend = L.control({position: 'bottomleft'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [23, 28, 30, 32, 35, 38],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+
+let popup;
+map.on("click", function(e) {
+  if (!popup) {
+    popup = L.popup()
+      .setLatLng([e.latlng.lat, e.latlng.lng])
+      .openOn(map);
+  } else {
+    popup.setLatLng([e.latlng.lat, e.latlng.lng]);
+  }
+  const value = image_layer.getValueAtLatLng(+e.latlng.lat, +e.latlng.lng);
+  popup.setContent(`Temperature: ${value.toFixed(2)}`).openOn(map);
+});
